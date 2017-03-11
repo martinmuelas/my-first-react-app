608bfd782fe35d6cb430b38630b0e4f743cd1755
Martín Muelas <martinmuelas@gmail.com>
2017-03-11 18:57:27 -0300

    Created first basic component

---
+++ src/components/StorePicker.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+  // Todo Component en React debe tener obligatoriamente un método
+  // render() 
+  render() {
+    return <p>Hello</p>
+  }
+}
+
+export default StorePicker;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
-// let's go!
-console.log('This is my first React App!');
+// Importamos librería React
+import React from 'react';
+// Importamos el método 'render' de la librería 'react-dom' 
+// (No necesitamos toda la librería)
+import { render } from 'react-dom';
+
+import StorePicker from './components/StorePicker'
+
+render(<StorePicker/>, document.querySelector('#main'));
