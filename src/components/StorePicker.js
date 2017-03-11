b714543e871fdce953e06d78a80cd4f00386736b
Martín Muelas <martinmuelas@gmail.com>
2017-03-11 20:56:05 -0300

    Created App, Header, Order and Inventory Components.Modified Header to a become Stateless Functional Component.

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -4,10 +4,6 @@ class StorePicker extends React.Component {
   render() {
     return (
       <form className="store-selector">
-        { /* Los comentarios se colocan de esta manera */}
-        { /* Y solo pueden estar despues de la etiqueta del */ }
-        { /* elemento que se devuelve. Recordar que React */ }
-        { /* no permite renderizar mas de un elemento padre */ }
         <h2>Please Enter A Store</h2>
         <input type="text" required placeholder="Store Name" />
         <button type="submit">Visit Store →</button>
