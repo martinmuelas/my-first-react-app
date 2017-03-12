c98c11621b6f8f15b7c69abd93cf3eb7a9795402
Martín Muelas <martinmuelas@gmail.com>
2017-03-11 22:48:35 -0300

    Introduced React Event to capture the name of the Store in the Store Picker. Created a constructor method to bind the 'this' object.

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,13 +2,33 @@ import React from 'react';
 import { getFunName } from '../helpers.js'
 
 class StorePicker extends React.Component {
+  // Tenemos que asociar el valor de 'this' al del componente
+  // por eso creamos un constructor
+  constructor() {
+    super(); // Genera primero el React Component, luego instancia el obj
+    this.goToStore = this.goToStore.bind(this);
+  }
+
+  goToStore(e) {
+    e.preventDefault();
+    console.log('Cambiaste la URL');
+    // Primero capturamos el texto del input
+    console.log(this.storeInput);
+    // Segundo cambiamos del '/' al '/store/:storeId'
+    
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
         <input type="text" required 
           placeholder="Store Name"
           defaultValue={getFunName()} 
+          // Vamos a crear una 'ref' para tener acceso al elemento
+          // desde otras partes del componente
+          // Antes se usaban String Refs, ahora son funciones:
+          ref={(input) => { this.storeInput = input }}
         />
         <button type="submit">Visit Store →</button>
       </form>
