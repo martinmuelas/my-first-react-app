fc9c0a3274582c5193f9d0f5aadb2d3900be52ae
Martín Muelas <martinmuelas@gmail.com>
2017-03-19 12:18:24 -0300

    Added PropTypes to validate data types in the components.

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,4 +16,19 @@ const Header = (props) => {
   )
 }
 
+// Utilizamos PropTypes como una manera de validar el tipo de datos
+// que deben recibir los components. Esto está pensado mas que nada
+// para trabajos colaborativos.
+// En caso de utilizar el componente pasandole datos con un tipo distinto
+// al especificado, emitirá un error por consola, indicando que no se
+// recibió el tipo de dato esperado. 
+Header.propTypes = {
+  tagline: React.PropTypes.string.isRequired
+}
+
+/*
+* Documentación sobre PropTypes:
+* https://facebook.github.io/react/docs/typechecking-with-proptypes.html
+ */
+
 export default Header;
--- src/components/Order.js
+++ src/components/Order.js
@@ -78,4 +78,10 @@ class Order extends React.Component {
   }
 }
 
+Order.propTypes = {
+  order: React.PropTypes.object.isRequired,
+  fishes: React.PropTypes.object.isRequired,
+  removeFromOrder: React.PropTypes.func.isRequired
+};
+
 export default Order;
