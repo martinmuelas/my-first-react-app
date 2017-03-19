411b913d09cd93a319ee1221a1ef70cb25c1e6fb
Martín Muelas <martinmuelas@gmail.com>
2017-03-19 11:11:46 -0300

    Added some React Component Animation to the order list.

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { formatPrice } from '../helpers'
+import CSSTransitionGroup from 'react-addons-css-transition-group';
 
 class Order extends React.Component {
   constructor() {
@@ -19,7 +20,18 @@ class Order extends React.Component {
     }
     return (
       <li key={key}>
-        <span>{count}lbs {fish.name} {removeButton}</span>
+        <span>
+          <CSSTransitionGroup
+            component="span"
+            className="count"
+            transitionName="count"
+            transitionEnterTimeout={250}
+            transitionLeaveTimeout={250}
+          >
+            <span key={count}>{count}</span>
+          </CSSTransitionGroup>
+          lbs {fish.name} {removeButton}
+        </span>
         <span className="price">{formatPrice(count * fish.price)}</span>
       </li>
     )
@@ -48,13 +60,19 @@ class Order extends React.Component {
     return (
       <div className="order-wrap">
         <h2>Your Order</h2>
-        <ul className="order">
+        <CSSTransitionGroup 
+          className="order"
+          component="ul"
+          transitionName="order"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={500}          
+        >
           {orderIds.map(this.renderOrder)}
           <li className="total">
             <strong>Total:</strong>
             {formatPrice(total)}            
           </li>
-        </ul>
+        </CSSTransitionGroup>
       </div>
     )
   }
